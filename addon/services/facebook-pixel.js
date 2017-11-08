66d34f744dc9e2bad055e0de071d9ae36f8d1876
Yemi <oluyemi@gmail.com>
2017-11-07 23:15:26 -0500

    Possible IE fix for fastboot

--- addon/services/facebook-pixel.js
+++ addon/services/facebook-pixel.js
@@ -19,7 +19,7 @@ export default Ember.Service.extend({
     }
   }),
 
-  track(event, params) {
+  track: function(event, params) {
     if (typeof fbq === 'undefined') {
       return null;
     }
